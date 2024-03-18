a1345b036ba54c2ee115127fbec72ad7e157a6f3
Oscar-Sanabria <sanabriaoscarjavier1@gmail.com>
2024-03-18 01:34:11 -0500

    first commit

---
+++ src/main/java/co/edu/uptc/distri1/Distri1Application.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.distri1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Distri1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Distri1Application.class, args);
+	}
+
+}
