086773502eae9ab6ae25d001b82bfddb1c0977af
Oscar-Sanabria <149726482+Oscar-Sanabria@users.noreply.github.com>
2024-04-03 19:43:44 -0500

    Update Distri1Application.java
    Distri1Application hereda de SpringBootServletInitializer para poder correr el .war

--- src/main/java/co/edu/uptc/distri1/Distri1Application.java
+++ src/main/java/co/edu/uptc/distri1/Distri1Application.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Distri1Application {
+public class Distri1Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Distri1Application.class, args);
 	}
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(Distri1Application.class);
+	}
 }
