98cea07b67f4acddc04853b29dc7c0eb42764932
Oscar-Sanabria <sanabriaoscarjavier1@gmail.com>
2024-04-03 20:28:09 -0500

    Modifiqué puerto

--- src/main/java/co/edu/uptc/distri1/controller/PersonController.java
+++ src/main/java/co/edu/uptc/distri1/controller/PersonController.java
@@ -1,7 +1,6 @@
 package co.edu.uptc.distri1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 import co.edu.uptc.distri1.model.Person;
 
 @RestController
+@RequestMapping("/persona")
 
 public class PersonController {
-    @PostMapping("/persona")
+    @GetMapping()
     public Person getPersonRandom() {
         Person person = new Person();
         person.setName("Oscar");
