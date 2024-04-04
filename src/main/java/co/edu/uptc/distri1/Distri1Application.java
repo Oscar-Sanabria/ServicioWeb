5e997347ef79058838b499366cb07785179dc48e
Oscar-Sanabria <149726482+Oscar-Sanabria@users.noreply.github.com>
2024-04-03 19:56:26 -0500

    Update Distri1Application.java
    Corrección de error por no importar SprinApplicationBuilder y SpringBootServletInitializer

--- src/main/java/co/edu/uptc/distri1/Distri1Application.java
+++ src/main/java/co/edu/uptc/distri1/Distri1Application.java
@@ -2,7 +2,8 @@ package co.edu.uptc.distri1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 @SpringBootApplication
 public class Distri1Application extends SpringBootServletInitializer{
 
