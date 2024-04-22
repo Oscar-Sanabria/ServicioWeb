9473efce23764b98b844f38177efe78a9d951bf4
Oscar-Sanabria <149726482+Oscar-Sanabria@users.noreply.github.com>
2024-04-22 17:33:04 -0500

    Update PersonController.java
    Agregué metodo para obtener el nombre del pc

--- src/main/java/co/edu/uptc/distri1/controller/PersonController.java
+++ src/main/java/co/edu/uptc/distri1/controller/PersonController.java
@@ -1,5 +1,7 @@
 package co.edu.uptc.distri1.controller;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,9 +15,17 @@ import co.edu.uptc.distri1.model.Person;
 public class PersonController {
     @GetMapping("/all")
     public Person getPersonRandom() {
+        String coname = "";
+        try {
+            InetAddress localHost = InetAddress.getLocalHost();
+            coname = localHost.getHostName();
+        } catch (UnknownHostException e) {
+            System.err.println("No se pudo obtener el nombre del equipo: " + e.getMessage());
+            return null;
+        }
         Person person = new Person();
         person.setName("Oscar");
-        person.setLastname("Sanabria");
+        person.setLastname(coname);
         return person;
     }
 }
